e2312bc451eb3cb5b3c5c0388f85442ad4167b80
Nerart <133236314+Nerart0@users.noreply.github.com>
2026-08-10 16:58:48 +0200

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-import os
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-class CORSHandler(SimpleHTTPRequestHandler):
-    def end_headers(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
-        self.send_header('Pragma', 'no-cache')
-        super().end_headers()
-HTTPServer(('0.0.0.0', 80), CORSHandler).serve_forever()
